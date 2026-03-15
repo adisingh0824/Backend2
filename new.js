dbd78980f3808a8182772a601d0ee54bfe529177
ADITYA SINGH <sadityasingh7990@gmail.com>
2026-03-16 00:27:51 +0530

    commit

---
+++ new.js
@@ -0,0 +1,31 @@
+// const fs=require('fs');
+// fs.appendFile("hey.txt","ma to achha hu", function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+// });
+// const fs=require('fs');
+// fs.rename("hey.txt","hello.txt", function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+// })qq
+// const fs=require('fs');
+// fs.copyFile("hello.txt","./copy /chacha.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+// fs.rmdir("hello.txt",function(err){
+//     if(err) console.log(err);
+//     else console.log("removed");
+// })
+// fs.readFile("./new.js","utf-8", function(err, data){
+//     if(err) console.error(err);
+//     else console.log(data);
+// });
+//HTTPS & HTTP
+
+const http=require('http');
+const server=http.createServer(function(req, res){
+    res.end("hello adi");
+})
+server.listen(3000);
+console.log("Listening on port 3000");
