b7ec589f12f44ce66be09b64000fa588738c6123
ADITYA SINGH <sadityasingh7990@gmail.com>
2026-03-16 22:58:48 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 //middleware
 app.use(function(req, res, next){
@@ -12,3 +12,56 @@ app.get('/profile', function (req, res){
     res.send('Hello from profile');
 })
 app.listen(3000);
+
+ */
+// var arr=[1,2,4,5];
+// //for each map filter find indexOf
+// arr.forEach(function(val){
+//     console.log(val + " hello");
+//
+// });
+//MAP
+ //var arr  =[1,2,3,4];
+//  var ans = arr.map(function(val){
+//      return 13;
+//  })
+// console.log(ans);
+// var ans =arr.filter(function(val){
+//     if(val>12){
+//         return true;
+//     }
+//     else return false;
+// })
+// console.log(ans);
+// var anss =arr.find(function(val){
+//     if(val === 2) return val;
+// })
+// console.log(anss);
+//////////OBJECTS {}
+// {
+//     name:"ADITYA",
+//     age: 25,
+//     kuchhbhi:kuchhbhi
+// }
+// key value pair
+// var obj ={
+//     name:"Adi",
+//     age:12
+// }
+// Object.freeze(obj);
+// obj.age=25;
+// var arr= [1,2,3,4]
+// function abcd(){
+//     return 12;
+// }
+// var ans =abcd();
+// console.log(ans);
+// var blob =await fetch(`https://randomuser.me/api/`);
+// var res= await blob.json();
+// console.log(res);
+  /// LINE BY LINE CODE CHALE ISEY    KHATE HAI SYNCHRONOUS
+////JO BHI C0DE ASYNC NATURE KA HO, USEY SIDE STACK MA BHEJ DO AGLE CODE KO CHALALO JO BHI SYNC KA NATURE KA HO, JAB BHI SAARE SYN C0DE CHAL JAAYE , TAB CHECK KARO KI ASYNC CODEN COMPLETE HUA YA NHI AND AGAR WO COMPLETE   HUA HO TO USEY MAIN STACK MAIN LAAO AND CHALA DO
+// async function abcd(){
+//     await fetch(`https://r5andomuser.me/appi/`);
+// }
+
