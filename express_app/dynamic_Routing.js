ea322e05ac4392c25b72f5e0624c26f93b503626
adisingh0824 <sadityasingh7990@gmail.com>
2026-03-21 21:46:29 +0530

    commit

---
+++ express_app/dynamic_Routing.js
@@ -0,0 +1,16 @@
+//  /profile/harshit
+//  /profile/harshita
+//  /profile/hardik
+//  /profile/rohit
+//  /profile/adi
+
+//   /profile/:username
+//Create a route /square/:num
+// that returns the square of the number.
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
