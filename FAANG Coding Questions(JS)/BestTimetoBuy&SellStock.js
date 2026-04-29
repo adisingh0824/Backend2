1bd31b02b3c292240a85dd7d64adfee1f47a1a46
adisingh0824 <sadityasingh7990@gmail.com>
2026-04-29 10:41:55 +0530

    commit

--- FAANG Coding Questions(JS)/BestTimetoBuy&SellStock.js
+++ FAANG Coding Questions(JS)/BestTimetoBuy&SellStock.js
@@ -1,9 +1,9 @@
-var maxProfit = function(prices) {
-    let min = prices[0], profit = 0;
-    for (let p of prices) {
-        min = Math.min(min, p);
-        profit = Math.max(profit, p - min);
-    }
-    return profit;
-};
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+// var maxProfit = function(prices) {
+//     let min = prices[0], profit = 0;
+//     for (let p of prices) {
+//         min = Math.min(min, p);
+//         profit = Math.max(profit, p - min);
+//     }
+//     return profit;
+// };
+// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
